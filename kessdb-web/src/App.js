45c07a1efef3186dbb0f9e19824118e5a8951010
kjone8371 <kjone8371@gmail.com>
2024-09-11 11:09:05 +0900

    1학년 동계해커톤

---
+++ kessdb-web/src/App.js
@@ -0,0 +1,17 @@
+import Router from "./Router.js"
+import Tutorial from './Tutorial/Tutorial';
+import Main from './Main.js';
+import './Main.css';
+function App() {
+  
+  
+  return (
+    <>
+        <div className = 'top-right-name'>KessDB</div>
+        <Router></Router>
+    </>
+  );
+}
+
+export default App;
+//<Link to={"tutorial"}>Go</Link>
---
+++ kessdb-web/src/Group.js
@@ -0,0 +1,20 @@
+import './Main.css';
+import { ReactComponent as Close } from './svg/Group 2.svg';
+
+
+
+export default function Group() {
+      
+  
+  return (
+    <>
+      <div className = 'group-page'>
+        <Close className='close'/>
+        <div className='home'>Home</div>
+        <div className='service'>Service</div>
+        <div className='tutorial'>Tutorial</div>
+        <div className='qa'>Q&A</div>
+      </div>
+    </>
+  );
+}
